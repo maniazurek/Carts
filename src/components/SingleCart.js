8c6568ff9c58b7c15f0bb2a4a2937c7f37135be3
Marianna Zurek <zurekmarianna@gmail.com>
2023-03-10 21:24:16 +0100

    added delete request

--- src/components/SingleCart.js
+++ src/components/SingleCart.js
@@ -1,13 +1,99 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useState } from "react";
+import { Button } from "../utils/styledComponents";
 
-const SingleCart = ({ id, listOfProducts }) => {
+const SingleCart = ({
+  id,
+  listOfProducts,
+  numberOfCategories,
+  handleOnDelete,
+}) => {
+  const [isCartOpened, setIsCartOpened] = useState(false);
+
+  const handleOpenCart = () => {
+    setIsCartOpened(true);
+  };
+
+  const deleteCart = (id) => {
+    handleOnDelete(id);
+    setIsCartOpened(false);
+  };
+
   return (
-    <div style={{ border: "1px solid red" }}>
-      <div>{id}</div>
-      {listOfProducts.map((product) => {
-        return <div>{product.title}</div>;
-      })}
-    </div>
+    <>
+      <div style={{ display: "flex", flexDirection: "column" }}>
+        <div
+          style={{
+            backgroundColor: "#fff",
+            borderRadius: "5px",
+            margin: "5px",
+            width: "500px",
+            padding: "5px",
+            overflowY: "auto",
+          }}
+        >
+          <div style={{ display: "flex", justifyContent: "space-between" }}>
+            <div>{id}</div>
+            <div>{numberOfCategories}</div>
+            <Button onClick={handleOpenCart} style={{ cursor: "pointer" }}>
+              Details
+            </Button>
+          </div>
+        </div>
+        {isCartOpened && (
+          <div
+            style={{
+              position: "absolute",
+              top: "0",
+              left: "0",
+              right: "0",
+              bottom: "0",
+              background: "rgba(0, 0, 0, 0.35)",
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <div
+              style={{
+                width: "700px",
+                height: "500px",
+                position: "relative",
+                background: "#fff",
+                borderRadius: "15px",
+                padding: "15px",
+              }}
+            >
+              {listOfProducts.map((product) => {
+                return <div key={product.id}>{product.title}</div>;
+              })}
+              <div
+                onClick={() => setIsCartOpened(false)}
+                style={{
+                  cursor: "pointer",
+                  position: "absolute",
+                  top: "15px",
+                  right: "15px",
+                }}
+              >
+                X
+              </div>
+              <Button
+                onClick={() => deleteCart(id)}
+                style={{
+                  position: "absolute",
+                  bottom: "15px",
+                  right: "15px",
+                  cursor: "pointer",
+                }}
+              >
+                Delete Cart
+              </Button>
+            </div>
+          </div>
+        )}
+      </div>
+    </>
   );
 };
 
--- src/pages/Main.js
+++ src/pages/Main.js
@@ -4,24 +4,58 @@ import useFetch from "../hooks/useFetch";
 import { BASE_API_URL } from "../utils/commons";
 
 const Main = () => {
-  const [cartsList, setCartsList] = useState([]);
-  const [cartsData, setCartsData] = useFetch(`${BASE_API_URL}`);
+  // const [cartsList, setCartsList] = useState([]);
+  // const [cartsData, setCartsData] = useFetch(`${BASE_API_URL}`);
+
+  // useEffect(() => {
+  //   if (cartsData.carts) {
+  //     setCartsList(cartsData.carts);
+  //   }
+  // }, [cartsData]);
+
+  const [cartsData, setCartsData] = useState([]);
 
   useEffect(() => {
-    if (cartsData.carts) {
-      setCartsList(cartsData.carts);
-    }
-  }, [cartsData]);
+    getCartsData();
+  }, []);
+
+  const getCartsData = () => {
+    fetch(`${BASE_API_URL}`)
+      .then((res) => res.json())
+      .then((data) => setCartsData(data.carts));
+  };
+
+  const handleOnDelete = (id) => {
+    fetch(`${BASE_API_URL}/${id}`, {
+      method: "DELETE",
+    })
+      .then((res) => res.json())
+      .then((resp) => getCartsData());
+  };
 
   return (
     <>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "space-between",
+          width: "500px",
+          padding: "5px",
+        }}
+      >
+        <div>ID</div>
+        <div>Number of categories</div>
+        <div>More</div>
+      </div>
       <div>
-        {cartsList.map((cart) => {
+        {cartsData.map((cart) => {
           return (
             <SingleCart
               key={cart.id}
               id={cart.id}
               listOfProducts={cart.products}
+              numberOfCategories={cart.products.length}
+              handleOnDelete={handleOnDelete}
             />
           );
         })}
